eb5a47a3451ca7cb1b1dc4c932651a6f86cb49b1
liaosifish <tessa20030807@hotmail.com>
2024-04-30 22:54:17 +0800

    upload

---
+++ src/layout/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center', letterSpacing: '1px' }">
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
